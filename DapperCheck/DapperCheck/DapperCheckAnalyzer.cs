e2c7992740338deb0a416d94ae9a4b4ac6a5b45f
George Vanburgh <george@vanburgh.me>
2018-01-28 18:47:16 +0000

    Initial implementation of DapperCheck
    Very rough - still to do:
    
    * Write tests
    * Use proper SQL parser
    * Suggest code fixes?

--- DapperCheck/DapperCheck/DapperCheckAnalyzer.cs
+++ DapperCheck/DapperCheck/DapperCheckAnalyzer.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -15,35 +14,93 @@ namespace DapperCheck
     {
         public const string DiagnosticId = "DapperCheck";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Naming";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(AnalyzeDapperCall, SyntaxKind.InvocationExpression);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private static string NormaliseColumnName(string givenColumnClause)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            var toReturn = givenColumnClause;
+            if (toReturn.Contains('.'))
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+                toReturn = toReturn.Split('.')[1];
+            }
 
+            if (toReturn.Contains("AS"))
+            {
+                toReturn = toReturn.Split(new string[] { "AS" }, StringSplitOptions.RemoveEmptyEntries)[1];
+            }
+
+            // TODO: Ewwww
+            if (toReturn.Contains("as"))
+            {
+                toReturn = toReturn.Split(new string[] { "as" }, StringSplitOptions.RemoveEmptyEntries)[1];
+            }
+
+            return toReturn.Trim();
+        }
+
+
+        // TODO: Use a proper SQL parser
+        private static List<string> GetColumnNamesFromSql(string givenSql)
+        {
+            var selectClause = givenSql.Split(new string[] { "FROM" }, StringSplitOptions.RemoveEmptyEntries).First().Substring(7);
+            return selectClause.Split(',').Select(s => s.Trim()).Select(NormaliseColumnName).ToList();
+        }
+
+        private static void AnalyzeDapperCall(SyntaxNodeAnalysisContext context)
+        {
+            var invocationExpression = (InvocationExpressionSyntax) context.Node;
+            var memberAccessExpr = invocationExpression.Expression as MemberAccessExpressionSyntax;
+            if (!memberAccessExpr?.Name.ToString().StartsWith("Query<") ?? true) return;
+
+            // Check we called the Typed query expression method
+            var memberSymbol = context.SemanticModel.GetSymbolInfo(memberAccessExpr).Symbol as IMethodSymbol;
+            if (memberSymbol?.ToString().StartsWith("System.Data.IDbConnection.Query<T>") ?? true) return;
+
+            // Check we have at least one argument
+            // TODO: Needed?
+            var argumentList = invocationExpression.ArgumentList as ArgumentListSyntax;
+            if ((argumentList?.Arguments.Count ?? 0) < 1) return;
+
+            // Get the compile-time value of the sql argument
+            var sqlLiteral = argumentList?.Arguments[0].Expression as IdentifierNameSyntax;
+            if (sqlLiteral == null) return;
+            var sqlOpt = context.SemanticModel.GetConstantValue(sqlLiteral);
+            if (!sqlOpt.HasValue) return; // Couldn't resolve the compile-time value
+            var sql = sqlOpt.Value as string;
+            if (string.IsNullOrEmpty(sql)) return;
+
+            // Get the type on the query generic
+            var deserializationTargetTypeSymbols = memberSymbol.TypeArguments;
+            if (deserializationTargetTypeSymbols == null) return; // This was not a typed call?
+            if (deserializationTargetTypeSymbols.Length != 1) return; // TODO: Support nested types??
+            var deserializationTargetTypeSymbol = deserializationTargetTypeSymbols.First() as INamedTypeSymbol;
+            if (deserializationTargetTypeSymbol == null) return; // TODO: Needed?
+
+            // Get the public parameters
+            // TODO: Need to check base class?
+            var deserializationTargetMemberNames = deserializationTargetTypeSymbol.MemberNames;
+
+            // Get the column names from SQL
+            var columns = GetColumnNamesFromSql(sql);
+
+            // Compare the two collections
+            var missingColumns = columns.Where(c => !deserializationTargetMemberNames.Contains(c));
+            foreach (var column in missingColumns)
+            {
+                // Raise alerts for missing column names
+                var diagnostic = Diagnostic.Create(Rule, sqlLiteral.GetLocation(), column, deserializationTargetTypeSymbol.Name);
                 context.ReportDiagnostic(diagnostic);
             }
         }
